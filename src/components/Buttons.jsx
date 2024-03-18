0dc255652ae06916028b94c4250448bb2c557020
jharr023 <jharringtonkc@gmail.com>
2024-03-18 18:00:17 -0500

    commit - task 4 - tests 1-5 passing

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,19 @@
+import satData from "./satData.jsx";
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
+  const handleFilterByAllOrbits = () => {
+    setSat(satData);
+  };
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      <div>
+        {displaySats.map((sat, id) => (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        ))}
+      </div>
+      <button onClick={handleFilterByAllOrbits}>All Orbits</button>
     </div>
   );
 };
